16e0dfe443c4d34359f037da399419a8f9b85551
henning.bunk <henning.bunk@wetteronline.de>
2020-09-25 17:07:00 +0200

    Layout: 9 Constraint Layout

---
+++ LayoutsCodelab/app/src/main/java/com/codelab/layouts/ui/ConstraintLayout.kt
@@ -0,0 +1,140 @@
+package com.codelab.layouts.ui
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.ConstraintSet
+import androidx.compose.foundation.layout.Dimension
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.WithConstraints
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+
+        // Create references for the composables to constrain
+        val (button1, button2, text) = createRefs()
+
+        Button(
+            onClick = { /* Do something */ },
+            // Assign reference "button" to the Button composable
+            // and constrain it to the top of the ConstraintLayout
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ) {
+            Text("Button 1")
+        }
+
+        Text("Text", Modifier.constrainAs(text) {
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerAround(button1.end)
+        })
+
+        val barrier = createEndBarrier(button1, text)
+        Button(
+            onClick = { /* Do something */ },
+            modifier = Modifier.constrainAs(button2) {
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(barrier)
+            }
+        ) {
+            Text("Button 2")
+        }
+
+    }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+    ConstraintLayout {
+        val text = createRef()
+
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text(
+            "This is a very very very very very very very very long text",
+            Modifier.constrainAs(text) {
+                linkTo(start = guideline, end = parent.end)
+                width = Dimension.preferredWrapContent
+//                preferredWrapContent - the layout is wrap content, subject to the constraints in that dimension.
+//                wrapContent - the layout is wrap content even if the constraints would not allow it.
+//                fillToConstraints - the layout will expand to fill the space defined by its constraints in that dimension.
+//                preferredValue - the layout is a fixed dp value, subject to the constraints in that dimension.
+//                value - the layout is a fixed dp value, regardless of the constraints in that dimension
+
+            }
+        )
+    }
+}
+
+@Composable
+fun DecoupleConstrainLayout() {
+    WithConstraints {
+        val constraints = if (minWidth < 600.dp) {
+            decoupledConstraints(margin = 16.dp)
+        } else {
+            decoupledConstraints(margin = 32.dp)
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { },
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text("Button")
+            }
+            Text(text = "Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin = margin)
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+    LayoutsCodelabTheme {
+        Surface {
+            ConstraintLayoutContent()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ConstraintLayoutPreview() {
+    LayoutsCodelabTheme {
+        Surface {
+            LargeConstraintLayout()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DecoupledConstraintLayoutPreview() {
+    LayoutsCodelabTheme {
+        Surface {
+            DecoupleConstrainLayout()
+        }
+    }
+}
