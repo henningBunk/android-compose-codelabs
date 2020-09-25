a2cc8ce32e97f56f03f8dc28833ba6291f85c7b8
henning.bunk <henning.bunk@wetteronline.de>
2020-09-25 09:03:01 +0200

    State: 7 Dynamic UI based on state

--- StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -78,6 +78,14 @@ fun TodoScreen(
 @Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("") }
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default)}
+    val iconsVisible = text.isNotBlank()
+
+    val submit = {
+        onItemComplete(TodoItem(text, icon))
+        setText("")
+        setIcon(TodoIcon.Default)
+    }
 
     Column {
         Row(Modifier
@@ -89,20 +97,22 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
                     onTextChange = setText,
                     modifier = Modifier
                             .weight(1f)
-                            .padding(end = 8.dp)
+                            .padding(end = 8.dp),
+                    onImeAction = submit
             )
             TodoEditButton(
-                    onClick = {
-                        onItemComplete(TodoItem(text))
-                        setText("")
-                    },
+                    onClick = submit,
                     text = "Add",
                     modifier = Modifier.align(Alignment.CenterVertically),
                     enabled = text.isNotBlank()
             )
         }
+        if (iconsVisible) {
+            AnimatedIconRow(icon = icon, onIconChange = setIcon, Modifier.padding(top = 8.dp))
+        } else {
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+        }
     }
-
 }
 
 /**
